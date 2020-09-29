67e1ff60bbd3cba14dbb7b2e6b1c8aed0d4c3900
CorrM <islamnofl.official@gmail.com>
2020-09-29 13:17:22 +0200

    - Setup `TdManager`.

--- dist/socialcord.plugin.js
+++ dist/socialcord.plugin.js
@@ -8,6 +8,32 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Socialcord = void 0;
 const bdapi_1 = { BdApi };
+const tdweb_1 = require("./socialcord/tdweb/tdweb");
+class TdManager {
+    constructor() {
+        const opts = {
+            logVerbosityLevel: 0,
+            jsLogVerbosityLevel: "error",
+            mode: "wasm",
+            instanceName: "tdlib",
+            readOnly: false,
+            isBackground: false,
+            useDatabase: false,
+            //wasmUrl: `${this.WASM_FILE_NAME}?_sw-precache=${this.WASM_FILE_HASH}`,
+            onUpdate: (update) => {
+                if (this.onUpdateCallBack)
+                    return this.onUpdateCallBack(update);
+            }
+        };
+        this.client = new tdweb_1.default(opts);
+    }
+    async Send(query) {
+        console.log(console.log('Send : ', query));
+        return this.client.send(query);
+    }
+}
+TdManager.WASM_FILE_NAME = 'a848b8b40a9281225b96b8d300a07767.wasm';
+TdManager.WASM_FILE_HASH = TdManager.WASM_FILE_NAME.replace('.wasm', '');
 class Socialcord {
     getName() {
         return "Socialcord";
@@ -21,8 +47,22 @@ class Socialcord {
     getAuthor() {
         return "CorrM";
     }
-    start() {
-        bdapi_1.BdApi.alert("CORRM", "Just check");
+    async start() {
+        bdapi_1.BdApi.alert("CORRM", bdapi_1.BdApi.React.version);
+        const tObj = {
+            "@type": "logOut",
+            "@extra": "sad"
+        };
+        const client = new TdManager();
+        client.onUpdateCallBack = (update) => console.log('UPDATE : ', update);
+        try {
+            const result = await client.Send(tObj);
+            console.log("Result : ", result);
+        }
+        catch (error) {
+            console.error(error);
+        }
+        console.log("FINISHED");
     }
     stop() {
     }
@@ -30,4 +70,5 @@ class Socialcord {
     }
 }
 exports.Socialcord = Socialcord;
+
 
--- src/socialcord.plugin.ts
+++ src/socialcord.plugin.ts
@@ -7,10 +7,42 @@
  */
 
 import { BdApi } from "@bandagedbd/bdapi";
+import TdClient, { TdError, TdObject, TdOptions } from "tdweb";
+
+class TdManager {
+    private static WASM_FILE_NAME = 'a848b8b40a9281225b96b8d300a07767.wasm';
+    private static WASM_FILE_HASH = TdManager.WASM_FILE_NAME.replace('.wasm', '');
+
+    private client: TdClient;
+    public onUpdateCallBack?: (update: TdObject) => any;
+
+    public constructor() {
+        const opts: TdOptions = {
+            logVerbosityLevel: 0,
+            jsLogVerbosityLevel: "error",
+            mode: "wasm",
+            instanceName: "tdlib",
+            readOnly: false,
+            isBackground: false,
+            useDatabase: false,
+            //wasmUrl: `${this.WASM_FILE_NAME}?_sw-precache=${this.WASM_FILE_HASH}`,
+            onUpdate: (update: TdObject) => {
+                if (this.onUpdateCallBack)
+                    return this.onUpdateCallBack(update);
+            }
+        };
+
+        this.client = new TdClient(opts);
+    }
+
+    public async Send(query: TdObject): Promise<TdError | TdObject> {
+        console.log(console.log('Send : ', query));
+        return this.client.send(query);
+    }
+}
 
 export class Socialcord
 {
-
     public getName(): string
     {
         return "Socialcord";
@@ -31,9 +63,25 @@ export class Socialcord
         return "CorrM";
     }
 
-    public start(): void
+    public async start(): Promise<void>
     {
-        BdApi.alert("CORRM", "Just check");
+        BdApi.alert("CORRM", BdApi.React.version);
+
+        const tObj: TdObject = {
+            "@type": "logOut",
+            "@extra": "sad"
+        };
+
+        const client = new TdManager();
+        client.onUpdateCallBack = (update: TdObject) => console.log('UPDATE : ', update);
+
+        try {
+            const result = await client.Send(tObj);
+            console.log("Result : ", result);
+        } catch (error) {
+            console.error(error);
+        }
+        console.log("FINISHED");
     }
 
     public stop(): void
